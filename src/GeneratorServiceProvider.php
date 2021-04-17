770c8247e5e0d2126216829f75e24b399bcfef74
maximiliian <johaneshamonangansilalahi@gmail.com>
2021-04-18 03:17:24 +0700

    stubs
    stubs

--- src/GeneratorServiceProvider.php
+++ src/GeneratorServiceProvider.php
@@ -1,2 +1,19 @@
-<php?
+<?php
 
+namespace christcalv\Generator;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class GeneratorServiceProvider extends ServiceProvider 
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        // 
+    }
+}
